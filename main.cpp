633f615832ab269770477fdd74faec23f4ba8b97
minfyjin <156170126+minfyjin@users.noreply.github.com>
2024-03-27 09:51:57 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-    qDebug("Hello World!");
+    qDebug("Hello World! i wait something ...");
     QTimer::singleShot(0, &a, &QCoreApplication::quit);
     return a.exec();
 }
